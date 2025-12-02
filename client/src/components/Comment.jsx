127304e94b60da9ea4973ad013c549d6b7fd9ac0
satyendra <ysatyendra1409@gmail.com>
2025-12-02 23:57:53 +0530

    api

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,7 @@
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
+    return (
+        <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
+            <p>Comment Placeholder</p>
+        </div>
+    );
+}

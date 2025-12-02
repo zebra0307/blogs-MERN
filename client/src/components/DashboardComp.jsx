18dc885e4ab4b4b0bd698348edb2579dc48b08fd
satyendra <ysatyendra1409@gmail.com>
2025-12-02 23:59:51 +0530

    api

---
+++ client/src/components/DashboardComp.jsx
@@ -0,0 +1,8 @@
+export default function DashboardComp() {
+    return (
+        <div className='p-3 md:mx-auto'>
+            <h1 className='text-center text-3xl my-7 font-semibold'>Dashboard Overview</h1>
+            <p>Dashboard Overview component placeholder</p>
+        </div>
+    );
+}
